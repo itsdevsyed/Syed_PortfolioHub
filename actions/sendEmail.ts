f7014dc657819b14296250cd9d46720e64f4122b
Syed Shoaib <syedshoaib8087@gmail.com>
2024-10-30 13:41:28 +0530

    added one updated resume and change the email according to the latest one

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -30,7 +30,7 @@ export const sendEmail = async (formData: FormData) => {
   try {
     data = await resend.emails.send({
       from: "Contact Form <onboarding@resend.dev>",
-      to: "syedshoaib8087@gmail.com",
+      to: "shoaib.syed4@outlook.com",
       subject: "Message from contact form",
       reply_to: senderEmail as string,
       text: message as string,     
